25ee13906a7fbe271e2a1edab641ed2d6d217dcd
Ashwary Pratap <ashwr3012@gmail.com>
2023-08-12 01:45:36 +0530

    added styles and states in moviecard.js

--- src/moviecard.js
+++ src/moviecard.js
@@ -1,20 +1,44 @@
 import React from "react";
 
 class MovieCard extends React.Component{
+    constructor(){
+        super();
+        this.state ={
+            title:"Oppenheimer",
+            plot: "During World War II, Lt. Gen. Leslie Groves Jr. appoints physicist J. Robert Oppenheimer to work on the top-secret Manhattan Project.",
+            prize: 1999,
+            rating:9.1,
+        }
+    }
     render(){
+        const {title,plot,prize,rating}=this.state;
+        
         return (
+
             <div className="main">
              <div className="movie-card">
                 <div className="left">
                     <img src="https://static.hd-trailers.net/images/-159759-poster-xlarge.jpg" alt="Poster"/>
                 </div>
                 <div className="right">
-                   <div className="title"> Oppenheimer </div>
-                   <div className="plot"> During World War II, Lt. Gen. Leslie Groves Jr. appoints physicist J. Robert Oppenheimer to work on the top-secret Manhattan Project. </div>
-                   <div className="prize"> Rs.1999/ only </div>  
+                   <div className="title"> {title} </div>
+                   <div className="plot"> {plot} </div>
+                   <div className="prize"> {prize} only </div>  
                    <div className="footer">
-                    <div className="rating">9.1</div>
-                    <div className="stars">star</div>
+                    <div className="rating">{rating}</div>
+                    <div className="stars-comp">
+                      
+                      <img className="star-btn" alt="decrease"
+                      src="https://cdn-icons-png.flaticon.com/128/56/56889.png"/>
+                      
+                      <img
+                      src="https://cdn-icons-png.flaticon.com/128/1828/1828884.png" 
+                      alt ="star" className="stars"/>
+                     <img className="star-btn" alt="increase"
+                      src="https://cdn-icons-png.flaticon.com/128/3524/3524388.png"/>  
+
+                     <span>0</span>      
+                    </div>
                     <button className="favourite-btn"> Favourite </button>
                     <button className="cart-btn">Add to cart</button>
                    </div>                 
